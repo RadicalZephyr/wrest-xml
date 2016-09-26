a34222a47d6548766a2869ae0eaa3590faeb4e1c
Geoff Shannon <geoffpshannon@gmail.com>
2016-09-25 22:08:42 -0500

    Add clojars as the release repository

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :deploy-repositories [["releases" :clojars]]
 
   :test-paths ["spec"]
 
